ca836ba4e831229b584ef7c59d850c9e4802a6fc
JWKennington <jameswkennington@gmail.com>
2019-04-06 23:29:49 -0500

    updating tests

--- tests/test_soln.py
+++ tests/test_soln.py
@@ -25,7 +25,7 @@ class SolutionGroupTest(unittest.TestCase):
             self.solns.with_meta_overrides(references_file=None).to_latex(tmp_file.as_posix())
             with open(tmp_file.as_posix(), 'r') as fid:
                 result = fid.read()
-                self.assertEqual(DEMO_TEX_FILE, result)
+                self.assertEqual(result, DEMO_TEX_FILE, msg="Expected:\n{}\n\nGot:\n{}".format(result, DEMO_TEX_FILE))
 
 
 DEMO_TEX_FILE = r"""
