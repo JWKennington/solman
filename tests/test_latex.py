32b876d9531051f61578eb9e2b18933eeda94c7d
JWKennington <jameswkennington@gmail.com>
2019-04-07 01:27:55 -0500

    rebaselines post docs

--- tests/test_latex.py
+++ tests/test_latex.py
@@ -19,7 +19,7 @@ class LatexTest(unittest.TestCase):
         self.solns = soln.SolutionGroup.from_meta(DEMO_ROOT / 'meta.yml')
 
     @unittest.skipIf(not LATEXMK_AVAILABLE, 'Must have pdflatex executable to test pdf compilation')
-    def test_to_pdf(self):
+    def test_render_pdf(self):
         with tempfile.TemporaryDirectory() as tmp:
             tmp = pathlib.Path(tmp)
             tmp_file = pathlib.Path(tmp) / 'demo_tmp.tex'
@@ -27,6 +27,17 @@ class LatexTest(unittest.TestCase):
             base_file = DEMO_ROOT / 'base_file.pdf'
             pdf_file = tmp / 'demo_file.pdf'
             latex.render_pdf(tmp_file.as_posix(), pdf_file)
+            base_checksum = pdf_file_checksum(base_file.as_posix())
+            new_checksum = pdf_file_checksum(pdf_file.as_posix())
+            self.assertEqual(new_checksum, base_checksum)
+
+    @unittest.skipIf(not LATEXMK_AVAILABLE, 'Must have pdflatex executable to test pdf compilation')
+    def test_to_pdf(self):
+        with tempfile.TemporaryDirectory() as tmp:
+            tmp = pathlib.Path(tmp)
+            base_file = DEMO_ROOT / 'base_file.pdf'
+            pdf_file = tmp / 'demo_file.pdf'
+            self.solns.to_pdf(pdf_file)
             base_checksum = pdf_file_checksum(base_file.as_posix())
             new_checksum = pdf_file_checksum(pdf_file.as_posix())
             self.assertEqual(new_checksum, base_checksum)
